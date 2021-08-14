4951095d8d4adbec51859769ef5e01275168c1f2
kaiorod <caiorodrigo2775@gmail.com>
2021-08-14 00:51:09 -0300

    finaliza musica

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Beep();
+            Console.BackgroundColor = ConsoleColor.DarkYellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Louco Mundão" + "\nNC da zl");
+            Console.ResetColor();
+
+Console.WriteLine(
+"\njoguei minha 'veinha' numa casa de rico" +
+"\ncorrendo com sede atrás do meu objetivo" +
+"\ncom paco no bolso eu fiquei até bonito" +
+"\nsabe que os 'menó' do morro não brinca em serviço" +
+"\n\nhoje em dia nego quer pagar até de amigo" +
+"\nsó que nas antigas nunca nem falou comigo" +
+"\npingente da cruz que me protege do perigo" +
+"\ne meu doze mola ta apavorando os bico");
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+Console.WriteLine(
+"\nacompanha o pique e não da chilique" +
+"\nta mordendo as costas de ver nós se levantar" +
+"\nmael no beat eu de bandit" +
+"\naquela que tem ronco que faz calcinha molhar" +
+"\n\nque saudade dos irmão que cumpriram sua missão" +
+"\ncuida de mim ai cima que eu to no corre nesse loco mundão" +
+"\nvou queima um do marrom pra amenizar os problema" +
+"\nvou caneta mais um som que isso vai vira minha fonte de renda" +
+"\n\nqueima" +
+"\nfogo em racista é o lema" +
+"\ntive muita persistência" +
+"\nsó pra cala boca desse sistema");
+Console.ResetColor();
+Console.WriteLine(
+"\n\nescalei meus fiel de fechar" +
+"\nsem moral pra quem quer se intrometer" +
+"\naqui a maldade rola até no ar não deixo ela me comprometer" +
+"\ne aqueles que tentam me parar" +
+"\nvou deixar um recado pra vocês" +
+"\n\nO NC ta 'brabão' de pegar se tentar a sorte vai virar freguês" +
+"\njoguei minha 'veinha' numa casa de rico" +
+"\ncorrendo com sede atrás do meu objetivo" +
+"\ncom paco no bolso eu fiquei até bonito" +
+"\nsabe que os 'menó' do morro não brinca em serviço" +
+"\n\nhoje em dia nego quer pagar até de amigo" +
+"\nsó que nas antigas nunca nem falou comigo" +
+"\npingente da cruz que me protege do perigo" +
+"\ne meu doze mola ta apavorando os bico");
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+Console.WriteLine(
+"\n\nacompanha o pique e não da chilique" +
+"\nta mordendo as costas de ver nós se levantar" +
+"\nmael no beat eu de bandit" +
+"\naquela que tem ronco que faz calcinha molhar" +
+"\n\nque saudade dos irmão que cumpriram sua missão" +
+"\ncuida de mim ai cima que eu to no corre nesse loco mundão" +
+"\nvou queima um do marrom pra amenizar os problema" +
+"\nvou caneta mais um som que isso vai vira minha fonte de renda" +
+"\n\nqueima" +
+"\nfogo em racista é o lema" +
+"\ntive muita persistência" +
+"\nsó pra cala boca desse sistema");
+Console.ResetColor();
+
+Console.WriteLine(
+"\n\nmelhor não conta com a sorte" +
+"\neles querem me ver no pó" +
+"\nlancei o jacaré da lacoste" +
+"\nsó pra combinar com meu short");
+        }
+    }
+}
